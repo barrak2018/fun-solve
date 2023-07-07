9a917e1ca62ea275cf299998279a587ea496f3c5
barrak2018 <joseestebanojedaaular@gmail.com>
2023-07-07 11:37:22 -0400

    3x3 upgrade pack

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 # C.I: V-30.300.275
 import matplotlib.pyplot as pp
 import os
+from numpy import linspace as lnsp
 
 
 # funciones
@@ -31,7 +32,7 @@ def quadraticFunction(a,b,c):
     #print (vertice)
     verticeY= a*(vertice**2) + b * vertice + c
     print('ingrese el numero de puntos pos y neg a graficar a partir del vertice o presione enter para graficar 10')
-    deltapos = input("tenga en cuenta que se grafica muy pocos puntos la grafica se vera mal >>> ")
+    deltapos = input(">>> ")
     if deltapos == "":
         deltapos = 10
     else:
@@ -50,13 +51,15 @@ def quadraticFunction(a,b,c):
 
     x = list(range(neglim,poslim +1))
     y = list (map(lambda x : a*(x**2) + b * x + c, x))
-
     tabla = list(zip(x,y))
     for index, tupla in enumerate (tabla):
         alpha, epsilon = tupla
         print(f"{index+1}: X={alpha} Y={epsilon}")
 
-    pp.plot(x, y)
+    x = lnsp(neglim,poslim,100)
+    def Fx(v):
+        return a*(v**2) + b * v + c
+    pp.plot(x, Fx(x))
     pp.xlabel("eje X")
     pp.ylabel('eje Y')
     pp.title("funcion cuadratica")
@@ -84,8 +87,42 @@ def linearFunction(dependiente = 1, independiente = 0):
     pp.show()
     input('pulse enter para continuar')
 
+# convertion 
+def conv ():
+    MuestraTitulo ()
+    print('1: binario a decimal \n2: decimal a binario')
+
+    mode = input('==> ')
+    n = int(input('ingrese el valor a convertir:  '))
+    _n = n
+    if mode == '1':
+        S = 0
+        i = 0
+        while n >= 1:
+            d = n % 10
+            if d < 0 or d > 1:
+                raise ValueError('not a binary')
+            n = int(n/10)
+            S = S + d * pow(2, i)
+            i += 1 
+        print(f'el binario {_n} corresponde al decimal {S}')
+    elif mode == "2":
+        d = []
+        i = 0
+        while n >= 1:
+            d.append(n % 2)
+            n = int(n/2)
+        S=""
+        for i in d[::-1]:
+            S = S + str(i)
+        MuestraTitulo ()
+        print(f'el Decimal {_n} corresponde al Binario {S}')
+    
+    input("pulse enter para continuar")
+
+
 def MuestraMenu():
-    menu = f"1: salir \n\r2: funcion lineal\n\r3: funcion cuadratica"
+    menu = f"1: salir \n\r2: funcion lineal\n\r3: funcion cuadratica\n4: Convercion de base"
     print(menu)
 
 
@@ -122,6 +159,12 @@ while (True):
             quadraticFunction(a,b,c)
         except:
             print('Sintax Err')
+            input('enter para continuar')
+    elif option == '4':
+        try:
+            conv()
+        except ValueError as Err:
+            print(f'Err: {Err}')
             input('enter para continuar')
     else:
         print('Error')
